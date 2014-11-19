cb89cae69116647f156a16256bf01b6e5812be14
Andrew Gillespie <gillespie.andrewc@gmail.com>
2014-11-19 11:05:10 -0600

    added views and partial for customer, routes to control access, and fun

--- app/controllers/finder_controller.rb
+++ app/controllers/finder_controller.rb
@@ -1,13 +1,18 @@
 class FinderController < ApplicationController
   def index
+    @title = "All Customers"
     @customers = Customer.all
   end
 
   def alphabetized
-    @customers = Customer.order(:name, :asc)
+    @title = "Alphabetized Customer List By Full Name"
+    @customers = Customer.order(:full_name, :asc)
+    render :index
   end
 
   def missing_email
+    @title = "Customers with Missing Emails"
     @customers = Customer.all.where(:email_address, nil)
+    render :index
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
+  get 'alphabetized' => 'finder/alphabetized' as 'alphabetized'
+
+  get 'missing_email' => 'finder/missing_email' as 'missing_email'
+
   root to: 'finder#index'
 end
